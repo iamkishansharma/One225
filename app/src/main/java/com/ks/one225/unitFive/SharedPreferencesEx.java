570fb33823fd0540d9ce6c7eb300fc4a90c9862f
Kishan Kr Sharma <skishan781@gmail.com>
2020-07-21 14:44:57 +0530

    Delete shared Preferences features and little ui fixed

--- app/src/main/java/com/ks/one225/unitFive/SharedPreferencesEx.java
+++ app/src/main/java/com/ks/one225/unitFive/SharedPreferencesEx.java
@@ -7,20 +7,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.ks.one225.R;
 
 public class SharedPreferencesEx extends AppCompatActivity {
 Button save;
-EditText name, address, mobileNo, email;
+TextInputEditText name, address, mobileNo, email;
 SharedPreferences mSharedPreferences;
 SharedPreferences.Editor mEditor;
+RadioGroup mRadioGroup;
+RadioButton o,b,r,g,bu;
+LinearLayout ll;
 boolean checkSaved;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +37,15 @@ boolean checkSaved;
         mobileNo = findViewById(R.id.edt_sp3);
         email = findViewById(R.id.edt_sp4);
 
+        mRadioGroup = findViewById(R.id.rg);
+        o = findViewById(R.id.orange_sp);
+        b = findViewById(R.id.black_sp);
+        r = findViewById(R.id.red_sp);
+        g = findViewById(R.id.green_sp);
+        bu = findViewById(R.id.blue_sp);
+        ll = findViewById(R.id.ll_sp);
         save = findViewById(R.id.btn_sp1);
+
         Toolbar toolbar = findViewById(R.id.sp_toolbar);
         setSupportActionBar(toolbar);
 
@@ -44,11 +58,12 @@ boolean checkSaved;
                 String setAddress = mSharedPreferences.getString("address", "");
                 String setMobileNo = mSharedPreferences.getString("mobileNo", "");
                 String setEmail = mSharedPreferences.getString("email", "");
-
+                int bgc = mSharedPreferences.getInt("bg",R.color.colorAccent );
                 name.setText(setName);
                 address.setText(setAddress);
                 mobileNo.setText(setMobileNo);
                 email.setText(setEmail);
+                ll.setBackgroundColor(getResources().getColor(bgc));
             }
         }
 
@@ -61,13 +76,26 @@ boolean checkSaved;
                 String mobileNo1 = mobileNo.getText().toString().trim();
                 String email1 = email.getText().toString().trim();
 
-                if(name1.equals("")&&address1.equals("")&&mobileNo1.equals("")&&email1.equals("")){
+                if(name1.isEmpty() &&address1.isEmpty()&&mobileNo1.isEmpty()&&email1.isEmpty()){
                     name.setError("Required Field !");
                     address.setError("Required Field !");
                     mobileNo.setError("Required Field !");
                     email.setError("Required Field !");
 
                 }else {
+                    switch (mRadioGroup.getCheckedRadioButtonId()){
+                        case R.id.orange_sp: mEditor.putInt("bg", android.R.color.holo_orange_dark);
+                            break;
+                        case R.id.black_sp: mEditor.putInt("bg", android.R.color.black);
+                            break;
+                        case R.id.red_sp: mEditor.putInt("bg", android.R.color.holo_red_dark);
+                            break;
+                        case R.id.green_sp: mEditor.putInt("bg", android.R.color.holo_green_dark);
+                            break;
+                        case R.id.blue_sp: mEditor.putInt("bg", android.R.color.holo_blue_dark);
+                            break;
+
+                    }
                     mEditor.putBoolean("data_saved", true);
                     mEditor.putString("name", name1);
                     mEditor.putString("address", address1);
@@ -75,6 +103,7 @@ boolean checkSaved;
                     mEditor.putString("email", email1);
                     mEditor.apply();
                     Toast.makeText(SharedPreferencesEx.this, "Your Details Saved !", Toast.LENGTH_LONG).show();
+                    finish();
                 }
             }
         });
@@ -96,6 +125,7 @@ boolean checkSaved;
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     deleteSharedPreferences("stored_data");
+                    finish();
                 }
             });
             builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
